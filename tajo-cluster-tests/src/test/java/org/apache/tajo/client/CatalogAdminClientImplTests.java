9dcabeee99ff39b1fd8b746db65a8024818a71c6
ariannaquinci <arianna.quinci99@gmail.com>
2023-07-04 19:28:52 +0200

    CatalogClientImplTests

--- tajo-cluster-tests/src/test/java/org/apache/tajo/client/CatalogAdminClientImplTests.java
+++ tajo-cluster-tests/src/test/java/org/apache/tajo/client/CatalogAdminClientImplTests.java
@@ -6,6 +6,7 @@ import org.apache.orc.OrcProto;
 import org.apache.tajo.*;
 import org.apache.tajo.annotation.Nullable;
 import org.apache.tajo.catalog.Schema;
+import org.apache.tajo.catalog.SchemaBuilder;
 import org.apache.tajo.catalog.TableMeta;
 import org.apache.tajo.catalog.partition.PartitionMethodDesc;
 import org.apache.tajo.catalog.proto.CatalogProtos;
@@ -17,10 +18,8 @@ import org.apache.tajo.util.CommonTestingUtil;
 import org.apache.tajo.util.KeyValueSet;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
-import org.junit.AfterClass;
+import org.junit.*;
 import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -42,65 +41,79 @@ public class CatalogAdminClientImplTests {
         private boolean expectedException;
         private String dbName;
 
-        private static  CatalogAdminClientImpl catalogAdminClient;
-        @BeforeClass
-        public static void init(){
+        private static CatalogAdminClientImpl catalogAdminClient;
 
-            catalogAdminClient= new CatalogAdminClientImpl(new SessionConnection(ServiceTrackerFactory.get(QueryTestCaseBase.getConf()), TajoConstants.DEFAULT_DATABASE_NAME,new KeyValueSet()));
+        @BeforeClass
+        public static void init() {
+
+            catalogAdminClient = new CatalogAdminClientImpl(new SessionConnection(ServiceTrackerFactory.get(QueryTestCaseBase.getConf()), TajoConstants.DEFAULT_DATABASE_NAME, new KeyValueSet()));
+
+        }
+
+        @After
+        public void clean() throws UndefinedDatabaseException, InsufficientPrivilegeException, CannotDropCurrentDatabaseException, IOException {
+             catalogAdminClient.dropDatabase(this.dbName);
 
         }
         @AfterClass
-        public static void clean() throws UndefinedDatabaseException, InsufficientPrivilegeException, CannotDropCurrentDatabaseException, IOException {
-            List<String> databaseNames=catalogAdminClient.getAllDatabaseNames();
-            for(String s: databaseNames){
-                catalogAdminClient.dropDatabase(s);
-            }
+        public static void clear() throws IOException {
             catalogAdminClient.close();
-
         }
-
 
 
         @Parameterized.Parameters
-        public static Collection<Object[][]> getParams(){
+        public static Collection<Object[][]> getParams() {
             return Arrays.asList(new Object[][]{
                     {"databaseExample", false},
-                    {"dataBase not valid name", false},
+                    {"dataBase with spaces name", false},
                     {null, false}
-                    });
+            });
         }
+
         //constructor
-        public createDataBaseTests(String dbName, boolean exc){
-            this.dbName=dbName;
-            this.expectedException=exc;
+        public createDataBaseTests(String dbName, boolean exc) {
+            this.dbName = dbName;
+            this.expectedException = exc;
         }
+
         @Test
-        public void createDataBaseTests(){
-            try{
+        public void createDataBaseTests() {
+            try {
                 catalogAdminClient.createDatabase(dbName);
                 Assert.assertTrue(catalogAdminClient.existDatabase(dbName));
             } catch (DuplicateDatabaseException e) {
+                Assert.assertTrue(this.expectedException);
+            }catch(NullPointerException e){
                 Assert.assertTrue(this.expectedException);
             }
         }
     }
 
     @RunWith(Parameterized.class)
-    public static class createExternalTableTests{
+    public static class createExternalTableTests {
         public static CatalogAdminClientImpl catalogAdminClient;
         private Class<? extends Exception> expectedException;
-       private  String tableName;
-       private Schema schema;
-       private PartitionMethodDesc partMethodDesc;
-       private TableMeta meta;
-       private URI path;
+        private String tableName;
+        private Schema schema;
+        private PartitionMethodDesc partMethodDesc;
+        private TableMeta meta;
+        private URI path;
+        private boolean created;
 
         @BeforeClass
         public static void init() throws IOException {
 
-            catalogAdminClient= new CatalogAdminClientImpl(new SessionConnection(ServiceTrackerFactory.get(QueryTestCaseBase.getConf()), TajoConstants.DEFAULT_DATABASE_NAME,new KeyValueSet()));
+            catalogAdminClient = new CatalogAdminClientImpl(new SessionConnection(ServiceTrackerFactory.get(QueryTestCaseBase.getConf()), TajoConstants.DEFAULT_DATABASE_NAME, new KeyValueSet()));
 
         }
+
+        @After
+        public void dropTable() throws UndefinedTableException, InsufficientPrivilegeException {
+            if (created) {
+                catalogAdminClient.dropTable(this.tableName);
+            }
+        }
+
         @AfterClass
         public static void clean() throws UndefinedDatabaseException, InsufficientPrivilegeException, CannotDropCurrentDatabaseException, IOException {
             //cancella la tabella esterne esistenti
@@ -111,68 +124,136 @@ public class CatalogAdminClientImplTests {
 
         //parameteres
         @Parameterized.Parameters
-        public static Collection<Object[][]> getParams() throws URISyntaxException, IOException {
-            Schema schemaMock= BackendTestingUtil.mockupSchema;
-            TableMeta metaMock= BackendTestingUtil.mockupMeta;
+        public static Collection<Object[][]> getParams() {
+            Schema schemaMock = BackendTestingUtil.mockupSchema;
+            TableMeta metaMock = BackendTestingUtil.mockupMeta;
 
 
             return Arrays.asList(new Object[][]{
-                   //tableName              schema               path           meta                 expectedException
-             //       {"table1",              schemaMock     ,     false,         metaMock,            UnavailableTableLocationException.class},
-               //     {"table1",              schemaMock     ,     true,          metaMock,            null},
-                 //   {"",                    schemaMock     ,     true,          metaMock,            null},
-                   // {null,                  schemaMock     ,     true,          metaMock,            NullPointerException.class},
-                    {"table2",              schemaMock      ,    false,          metaMock,           InsufficientPrivilegeException.class}
+                    //tableName              schema               path           meta         partMethodDescValid        expectedException
+                    {"table1", schemaMock, false, metaMock, true, UnavailableTableLocationException.class},
+                    {"table1", schemaMock, true, metaMock, true, null},
+                    {"", schemaMock, true, metaMock, true, null},
+                    {null, schemaMock, true, metaMock, true, NullPointerException.class},
+                    {"table1", null, true, metaMock, true, NullPointerException.class},
+                    {"table1", schemaMock, true, null, true, NullPointerException.class},
 
-
+                    {"table 1", schemaMock, true, metaMock, true, null},
+                    {"table1", schemaMock, true, metaMock, false, NullPointerException.class},
+                    //pensavo che il seguente test portasse IllegalArgumentException invece la creazione della tabella va a buon fine
+                    {"table1", schemaMock, true, metaMock, false, IllegalArgumentException.class},
 
 
             });
         }
+
         //constructor
-        public createExternalTableTests(String tableName, Schema schema, boolean path, TableMeta meta,  Class<?extends Exception> expectedExc) throws URISyntaxException, IOException {
-           this.tableName=tableName;
-           this.schema= schema;
+        public createExternalTableTests(String tableName, Schema schema, boolean path, TableMeta meta, boolean partMethodDescValid, Class<? extends Exception> expectedExc) throws URISyntaxException, IOException {
+            this.tableName = tableName;
+            this.schema = schema;
             TajoTestingCluster cluster = TpchTestBase.getInstance().getTestingCluster();
             TajoConf conf = cluster.getConfiguration();
-           if(path){
+            if (path) {
 
-               Path tPath=StorageUtil.concatPath(CommonTestingUtil.getTestDir(), "table1");
-               BackendTestingUtil.writeTmpTable(conf, tPath);
-               this.path= tPath.toUri();
-           }else{
-               if(expectedExc.equals(UnavailableTableLocationException.class)){
-               this.path= new URI("/target/example");
-               }else if(expectedExc.equals(InsufficientPrivilegeException.class)){
-                   this.path=TajoConf.getWarehouseDir(conf).toUri();
-               }
+                Path tPath = StorageUtil.concatPath(CommonTestingUtil.getTestDir(), "table1");
+                BackendTestingUtil.writeTmpTable(conf, tPath);
+                this.path = tPath.toUri();
+            } else {
+                this.path = new URI("/target/example");
+            }
 
-           }
-           this.meta=meta;
 
-           this.expectedException=expectedExc;
-           this.partMethodDesc=new PartitionMethodDesc("database", this.tableName, CatalogProtos.PartitionType.COLUMN, "expression", this.schema);
+            this.meta = meta;
 
+            this.expectedException = expectedExc;
+            if (partMethodDescValid) {
+                this.partMethodDesc = new PartitionMethodDesc("database", this.tableName, CatalogProtos.PartitionType.COLUMN, "expression", this.schema);
+            } else {
+                if(expectedExc.equals(IllegalArgumentException.class)) {
+                    this.partMethodDesc = new PartitionMethodDesc("database", this.tableName + "bis", CatalogProtos.PartitionType.COLUMN, "expression", this.schema);
+                }else{
+                    this.partMethodDesc=null;
+                }
+            }
         }
 
 
-            @Test
-        public void createExternalTableTests(){
+        @Test
+        public void createExternalTableTests() {
 
-            try{
-                Assert.assertEquals("default."+tableName, catalogAdminClient.createExternalTable(tableName,schema,path, meta, this.partMethodDesc).getName());
-
-            }catch (UnavailableTableLocationException e) {
-                Assert.assertEquals( this.expectedException, e.getClass());
+            try {
+                Assert.assertEquals("default." + tableName, catalogAdminClient.createExternalTable(tableName, schema, path, meta, this.partMethodDesc).getName());
+                created = true;
+            } catch (UnavailableTableLocationException e) {
+                Assert.assertEquals(this.expectedException, e.getClass());
             } catch (InsufficientPrivilegeException e) {
-                Assert.assertEquals( this.expectedException, e.getClass());
+                Assert.assertEquals(this.expectedException, e.getClass());
             } catch (DuplicateTableException e) {
                 Assert.assertEquals(this.expectedException, e.getClass());
-            }catch(NullPointerException e){
+            } catch (NullPointerException e) {
                 Assert.assertEquals(this.expectedException, e.getClass());
             }
         }
 
 
     }
+
+    @RunWith(Parameterized.class)
+    public static  class dropDatabaseTests{
+        private Class<?extends Exception> expectedException;
+        private String dbName;
+
+        private static CatalogAdminClientImpl catalogAdminClient;
+
+        @BeforeClass
+        public static void init() throws DuplicateDatabaseException {
+
+            catalogAdminClient = new CatalogAdminClientImpl(new SessionConnection(ServiceTrackerFactory.get(QueryTestCaseBase.getConf()), TajoConstants.DEFAULT_DATABASE_NAME, new KeyValueSet()));
+            catalogAdminClient.createDatabase("default_database");
+        }
+
+
+        @AfterClass
+        public static void clear() throws IOException {
+            catalogAdminClient.close();
+        }
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[][]> getParams() {
+            return Arrays.asList(new Object[][]{
+                    {"default_database", null},
+                    {"dataBaseNonEsistente", UndefinedDatabaseException.class},
+                    {null, UndefinedDatabaseException.class}
+            });
+        }
+
+        //constructor
+        public dropDatabaseTests(String dbName, Class<? extends Exception> exc) {
+            this.dbName = dbName;
+            this.expectedException = exc;
+        }
+
+        @Test
+        public void dropDataBaseTests() {
+            try {
+                catalogAdminClient.dropDatabase(dbName);
+                Assert.assertFalse(catalogAdminClient.existDatabase(dbName));
+            } catch (UndefinedDatabaseException e) {
+                Assert.assertEquals(this.expectedException, e.getClass());
+
+            } catch (InsufficientPrivilegeException e) {
+                Assert.assertEquals(this.expectedException, e.getClass());
+
+            } catch (CannotDropCurrentDatabaseException e) {
+                Assert.assertEquals(this.expectedException, e.getClass());
+
+            }
+        }
+
+    }
+
+
+
+
 }
